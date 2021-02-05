fdd123407c09691f923c1c013b934b1e89f682cd
Darsh57 <72237317+Darsh57@users.noreply.github.com>
2021-02-05 12:17:39 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var database, position;
+var balloon, back;
+
+function preload(){
+  backImg = loadImage("images/back.png");
+  balloonImg = loadImage("images/balloonpic.png");
+}
+
+function setup() {
+  database = firebase.database();
+  createCanvas(1000, 750);
+  balloon = createSprite(300, 400, 20, 20);
+  balloon.addImage(balloonImg);
+
+  var balloonpos = database.ref("balloon/position");
+  balloonpos.on("value", readPosition);
+}
+
+function draw() {
+  background(backImg);  
+
+  if(position!==undefined){
+
+    if(keyDown(LEFT_ARROW)){
+      changePosition(-1, 0);
+     } 
+     else if(keyDown(RIGHT_ARROW)){
+       changePosition(+1, 0);
+     } 
+     else if(keyDown(DOWN_ARROW)){
+       changePosition(0, +1);
+     } 
+     else if(keyDown(UP_ARROW)){
+       changePosition(0, -1);
+     }
+   
+  }
+  drawSprites();
+}
+
+function changePosition(){
+database.ref("balloon/position").set(
+  {
+    "x": position.x + x, 
+    "y": position.y + y
+}
+)
+}
+
+function readPosition(data){
+  position = data.val();
+  balloon.x = position.x;
+  balloon.y = position.y;
+}
